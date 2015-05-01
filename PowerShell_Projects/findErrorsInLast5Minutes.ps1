7bca08b38332bc8cd230375dc1828dd01f10a511
w3bguy <cbastian313@gmail.com>
2015-05-01 14:08:27 -0500

    Added findErrorsInLast5Minutes.ps1

---
+++ PowerShell_Projects/findErrorsInLast5Minutes.ps1
@@ -0,0 +1,14 @@
+﻿# 
+# Author: Charles Bastian
+#         Charles@CharlesBastian.com
+# Date Created: 2015-04-17
+# File Name: findErrorsInLast5Minutes.ps1
+# Description: PowerShell script to find "ERROR" in log files.
+#
+Get-ChildItem -Path C:\PROGRA~2\IBM\WEBSPH~1\errors\ -Filter "*.fdc" | 
+  Where-Object {$_.LastWriteTime -gt (Get-Date).AddMinutes(-1) -AND $_.Attributes -ne "Directory"} | 
+    ForEach-Object{
+		  if(Get-Content $_.FullName | Select-String -Pattern "ERROR"){
+			  exit 5
+			}
+		}
---
+++ PowerShell_Projects/keyPress_ChromeTabRotate.ps1
@@ -0,0 +1,13 @@
+﻿# 
+# Author: Charles Bastian
+#         Charles@CharlesBastian.com
+# Date Created: 2015-04-28
+# File Name: keyPress_ChromeTabRotate.ps1
+# Description: PowerShell script to rotate through the tabs of an open Chrome browser.
+#
+while(1 -eq 1){
+  $wshell=New-Object -ComObject wscript.shell;
+  $wshell.AppActivate('Google Chrome');
+  Sleep 5;
+  $wshell.SendKeys('^{PGDN}');
+} 
